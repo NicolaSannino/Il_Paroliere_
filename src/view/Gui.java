8101e7fe7102c775cabf691aee73b3265419c937
AlbertoTarabella <alberto.tarabella@edu.cobianchi.it>
2023-02-14 11:36:26 +0100

    commit libreire gui 1

--- src/view/Gui.java
+++ src/view/Gui.java
@@ -1,4 +1,20 @@
 package view;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Gui {
+
 }
