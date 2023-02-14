2d8827b563033cf13d30fecc25403c92b5eb839c
nicola.sannino <nicola10106@gmail.com>
2023-02-14 11:57:41 +0100

    Creazione Jframe e istanziarlo dal Main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             Gui window = new Gui();
+            window.getFrame().setExtendedState(window.getFrame().MAXIMIZED_BOTH);
             window.getFrame().setVisible(true);
         } catch (Exception e) {
             e.printStackTrace();
