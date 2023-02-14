a7254e3dd5fde60f41f3e12b4441ec7953124ca8
nicola.sannino <nicola10106@gmail.com>
2023-02-14 12:16:39 +0100

    Creazione Jframe e istanziarlo dal Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
-import view.Gui;
+import view.Gui1;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Gui window = new Gui();
+            Gui1 window = new Gui1();
             window.getFrame().setExtendedState(window.getFrame().MAXIMIZED_BOTH);
             window.getFrame().setVisible(true);
         } catch (Exception e) {
--- src/view/Gui1.java
+++ src/view/Gui1.java
@@ -1,6 +1,4 @@
 package view;
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Frame;
 import java.awt.GridLayout;
@@ -8,23 +6,22 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
+import javax.swing.*;
 
 import javax.swing.JFrame;
 import java.awt.EventQueue;
-
-public class Gui {
+public class Gui1 {
     private JFrame frame;
+    private JTextArea textArea1;
+    private JPanel panel1;
 
-    public Gui() {
+    public Gui1() {
         frame = new JFrame();
         frame.setBounds(100, 100, 450, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel lblNewLabel = new JLabel("Il paroliere");
+        lblNewLabel.setBounds(181, 33, 53, 14);
+        frame.getContentPane().add(lblNewLabel);
     }
 
     public JFrame getFrame() {
@@ -34,4 +31,5 @@ public class Gui {
     public void setFrame(JFrame frame) {
         this.frame = frame;
     }
+
 }
