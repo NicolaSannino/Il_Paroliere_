4565fa944790e5531db6f9837b865b7797abf06f
AlbertoTarabella <alberto.tarabella@edu.cobianchi.it>
2023-02-15 10:32:16 +0100

    commit prova matrice ciclo for

--- src/view/Gui.java
+++ src/view/Gui.java
@@ -18,20 +18,28 @@ import javax.swing.JTextField;
 import javax.swing.JFrame;
 import java.awt.EventQueue;
 
-public class Gui {
+public class Gui extends JFrame implements ActionListener{
     private JFrame frame;
-
+    private JLabel Matrice;
     public Gui() {
         frame = new JFrame();
         frame.setBounds(100, 100, 450, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        for(int i=1;i<5;i++){
+            for (int j=1;j<5;j++){
+                Matrice=new JLabel(String.valueOf(i*j));
+                Matrice.setBounds(i*50,j*50,50,50);
+                frame.add(Matrice);
+            }
+        }
     }
-
     public JFrame getFrame() {
         return frame;
     }
-
     public void setFrame(JFrame frame) {
         this.frame = frame;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
     }
 }
