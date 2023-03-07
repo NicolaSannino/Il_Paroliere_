0fa975efad8ee2847cfaf1bee850f4cec1220c12
AndreaComolli <andre.comolli@edu.cobianchi.it>
2023-03-07 11:19:33 +0100

    seconda matrice

--- src/view/Gui1.java
+++ src/view/Gui1.java
@@ -1,36 +1,34 @@
 package view;
-import java.awt.Font;
-import java.awt.Frame;
-import java.awt.GridLayout;
-import java.awt.Toolkit;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 import javax.swing.JFrame;
-import java.awt.EventQueue;
+
 public class Gui1 extends JFrame implements ActionListener{
     private JFrame frame;
     private JTextArea textArea1;
-    private JPanel panel1;
+    private JPanel panel;
     private JLabel Matrice;
 
     public Gui1() {
-        frame = new JFrame();
-        frame.setBounds(100, 100, 1000, 1000);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel lblNewLabel = new JLabel("Il paroliere");
-        lblNewLabel.setBounds(181, 33, 53, 14);
-        frame.getContentPane().add(lblNewLabel);
-
-        for(int i=1;i<5;i++){
-            for (int j=1;j<5;j++){
-                Matrice=new JLabel(String.valueOf(i*j));
-                Matrice.setBounds(i*50,j*50,50,50);
-                frame.add(Matrice);
-            }
+        setTitle("Matrice 4x4");
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(4, 4));
+        for (int i = 1; i <= 16; i++) {
+            JLabel label = new JLabel(Integer.toString(i));
+            label.setHorizontalAlignment(JLabel.CENTER);
+            label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            panel.add(label);
         }
+        add(panel);
+        setVisible(true);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
