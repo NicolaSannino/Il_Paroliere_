a6cfc5e50cb6d2ebb137a793eda533f50e9e1235
AndreaComolli <andre.comolli@edu.cobianchi.it>
2023-04-07 02:29:06 +0200

    implementazione matrice in campo gioco 3

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import view.HomePage;
 public class Main {
     public static void main(String[] args) {
         try {
+
             HomePage window1 = new HomePage();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
