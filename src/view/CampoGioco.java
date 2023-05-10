9ffcff71e94a7ef04c313b20a0115d392743f5bc
nicola.sannino <nicola10106@gmail.com>
2023-05-10 22:10:59 +0200

    Funzioni per cercare la parola all'interno della tabella e gestione bottoni cliccabili per inserire la parola

--- src/view/CampoGioco.java
+++ src/view/CampoGioco.java
@@ -7,7 +7,9 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
+import java.util.Iterator;
 import java.util.Random;
+import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -19,10 +21,13 @@ import model.Query;
 
 
 public class CampoGioco extends JFrame implements ActionListener{
+	Vector<JButton> buttonsclick=new Vector(0,1);
+
+	JButton[][] buttons = new JButton[4][4];
 
 	private char[][] tabella = new char[4][4];
 	private JLabel Testo;
-	private JTextField testo;
+	//private JTextField testo;
 
 	private JButton Tabella, Invio;
 	private JPanel GrigliaGioco;
@@ -57,7 +62,6 @@ public class CampoGioco extends JFrame implements ActionListener{
 		char[] consonanti = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'z'};
 		char[] vocali = {'a', 'e', 'i', 'o', 'u'};
 		int a=0;
-
 		for(int i = 1;i < 5;i++){
 			a++;
 			for(int j = 1;j < 5; j++){
@@ -72,6 +76,7 @@ public class CampoGioco extends JFrame implements ActionListener{
 
 				Tabella = new JButton(""+(char)num);
 				tabella[i-1][j-1]=(char)num;
+				buttons[i-1][j-1] = Tabella;
 				Tabella.setName("Btn"+a);
 				Tabella.addActionListener(this);
 				Tabella.setSize(50, 50);
@@ -90,25 +95,25 @@ public class CampoGioco extends JFrame implements ActionListener{
 
 		Border border = BorderFactory.createLineBorder(Color.blue, 3);
 
-		//Testo = new JLabel("Inerisci Lettere");
-		testo= new JTextField("Inserisci lettere");
+		Testo = new JLabel("Inserisci Lettere");
+		//testo= new JTextField("Inserisci lettere");
 
-		testo.setOpaque(false);
-		//this.add(Testo);
-		this.add(testo);
+		//testo.setOpaque(false);
+		this.add(Testo);
+		//this.add(testo);
 
 		Font fontA = new Font ("Book Antiqua", Font. BOLD, 15);
 
-		testo.setBorder(border);
+		Testo.setBorder(border);
 		//testo.setVerticalAlignment(labelTitolo.CENTER);
 		//testo.setHorizontalAlignment(labelTitolo.CENTER);
 		//testo.setSize(200,40);
 		//testo.setBackground(new Color(0, 0, 255));
 		//testo.setBounds(684,560,130,31);
-		testo.setSize(230,30);
-		testo.setFont(fontA);
-		testo.setForeground(Color.white);
-		this.centerComponent(testo, 550);
+		Testo.setSize(230,30);
+		Testo.setFont(fontA);
+		Testo.setForeground(Color.white);
+		this.centerComponent(Testo, 550);
 
 		Invio = new JButton("Cerca Parola");
 		this.add(Invio);
@@ -166,32 +171,52 @@ public class CampoGioco extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		String azione=e.getActionCommand();
+		boolean trovato=false;
+		String azione = e.getActionCommand();
+		JButton button = (JButton) e.getSource();
+		buttonsclick.add(button);
 
-		for(int i=97;i<123;i++){
-			i=(char)i;
-			if(azione.equals(Character.toString(i))){
-				if(testo.getText().equals("Inserisci lettere")){
-					testo.setText("");
-					testo.setText(testo.getText()+azione);
-				}else{
-					testo.setText(testo.getText()+azione);
+		for (int i = 97; i < 123; i++) {
+			i = (char) i;
+			if (azione.equals(Character.toString(i))) {
+				if (Testo.getText().equals("Inserisci Lettere")) {
+					Testo.setText("");
+					Testo.setText(Testo.getText() + azione);
+				} else {
+					Testo.setText(Testo.getText() + azione);
+				}
+				for(int n=0;n<4 && trovato==false;n++){
+					for(int j=0;j<4 && trovato==false;j++){
+						if(buttons[n][j].equals(button)){
+							updateButtons(n,j);
+							trovato=true;
+						}
+					}
 				}
 			}
 		}
-		if(azione.equals("Cerca Parola")){
-			System.out.println(testo.getText());
+		if (azione.equals("Cerca Parola")) {
+			System.out.println(Testo.getText());
 			DBConnectionMariaDB conn = new DBConnectionMariaDB();
-			Query q=new Query();
-			try {
-				if(q.ricercaParolaDb(testo.getText(),conn) == true){
-					System.out.println("parola trovata");
-				}else{
-					System.out.println("parola non trovata");
+			Query q = new Query();
+
+
+			if (controllaParola(Testo.getText()) == true) {
+				System.out.println("parola trovata");
+				try {
+					if(q.ricercaParolaDb(Testo.getText(),conn) == true){
+						System.out.println("parola trovata");
+					}else{
+						System.out.println("parola non trovata");
+					}
+				} catch (SQLException ex) {
+					throw new RuntimeException(ex);
 				}
-			} catch (SQLException ex) {
-				throw new RuntimeException(ex);
+
+			} else {
+				System.out.println("parola non trovata");
 			}
+
 
 		}
 	}
@@ -203,5 +228,141 @@ public class CampoGioco extends JFrame implements ActionListener{
 				System.out.println(tabella[i][j]);
 			}
 		}
+	}
+
+	public boolean controllaParola(String parola) {
+		// Controlla che la parola non sia vuota o null
+		if (parola == null || parola.length() == 0) {
+			return false;
+		}
+
+		// Converte la parola in minuscolo per confrontare i caratteri
+		parola = parola.toLowerCase();
+
+		// Cerca la prima lettera della parola nella tabella
+		for (int riga = 0; riga < tabella.length; riga++) {
+			for (int colonna = 0; colonna < tabella[0].length; colonna++) {
+				if (tabella[riga][colonna] == parola.charAt(0)) {
+					// Se la prima lettera è stata trovata, cerca le lettere successive
+					if (cercaParola(parola, riga, colonna, 1)) {
+						return true;
+					}
+				}
+			}
+		}
+
+		// Se la parola non è stata trovata, restituisce false
+		return false;
+	}
+
+	private boolean cercaParola(String parola, int riga, int colonna, int indice) {
+		// Se tutte le lettere della parola sono state trovate, restituisce true
+		if (indice == parola.length()) {
+			return true;
+		}
+
+		// Cerca la lettera successiva nella tabella
+		for (int riga2 = riga - 1; riga2 <= riga + 1; riga2++) {
+			for (int colonna2 = colonna - 1; colonna2 <= colonna + 1; colonna2++) {
+				// Controlla che la posizione sia all'interno della tabella
+				if (riga2 >= 0 && riga2 < tabella.length && colonna2 >= 0 && colonna2 < tabella[0].length && tabella[riga2][colonna2] == parola.charAt(indice)) {
+					// Se la lettera successiva è stata trovata, cerca le lettere successive
+					if (cercaParola(parola, riga2, colonna2, indice + 1)) {
+						return true;
+					}
+				}
+			}
+		}
+
+		// Se la parola non è stata trovata, restituisce false
+		return false;
+	}
+
+	private void updateButtons(int row, int col) {
+		// Impostazione della cliccabilità di tutti i bottoni a false
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				buttons[i][j].setEnabled(false);
+			}
+		}
+		// Impostazione della cliccabilità dei bottoni adiacenti alla cella selezionata a false
+		if (row > 0 ) {
+			if(ifBottoneGiaCliccato(buttons[row - 1][col])==false) {
+				buttons[row - 1][col].setEnabled(true); // Cellula sopra
+			}
+
+			if(col>0){
+				if(col < 3){
+					if(ifBottoneGiaCliccato(buttons[row -1][col +1 ])==false){
+						buttons[row - 1][col + 1].setEnabled(true); // Cellula sopra
+						if(ifBottoneGiaCliccato(buttons[row - 1][col - 1 ])==false){
+							buttons[row - 1][col - 1].setEnabled(true); // Cellula sopra
+						}
+					}
+				}else{
+					if(ifBottoneGiaCliccato(buttons[row - 1][col - 1 ])==false){
+						buttons[row - 1][col - 1].setEnabled(true); // Cellula sopra
+					}
+				}
+			}else{
+				if(ifBottoneGiaCliccato(buttons[row +1][col + 1 ])==false){
+					buttons[row - 1][col + 1].setEnabled(true); // Cellula sopra
+				}
+			}
+
+		}
+		if (row < 3) {
+			if(ifBottoneGiaCliccato(buttons[row + 1][col])==false){
+				buttons[row + 1][col].setEnabled(true); // Cellula sotto
+			}
+
+			if(col>0){
+				if(col < 3){
+					if(ifBottoneGiaCliccato(buttons[row +1][col +1 ])==false){
+						buttons[row + 1][col + 1].setEnabled(true); // Cellula sopra
+						if(ifBottoneGiaCliccato(buttons[row +1][col - 1 ])==false){
+							buttons[row + 1][col - 1].setEnabled(true); // Cellula sopra
+						}
+					}
+				}else{
+					if(ifBottoneGiaCliccato(buttons[row +1][col - 1 ])==false){
+						buttons[row + 1][col - 1].setEnabled(true); // Cellula sopra
+					}
+				}
+			}else{
+				if(ifBottoneGiaCliccato(buttons[row +1][col + 1 ])==false){
+					buttons[row + 1][col + 1].setEnabled(true); // Cellula sopra
+				}
+			}
+		}else{
+
+		}
+		if (col > 0) {
+			if(ifBottoneGiaCliccato(buttons[row][col - 1])==false){
+				buttons[row][col - 1].setEnabled(true); // Cellula a sinistra
+			}
+		}
+		if (col < 3) {
+			if(ifBottoneGiaCliccato(buttons[row][col + 1])==false){
+				buttons[row][col + 1].setEnabled(true); // Cellula a destra
+			}
+		}
+
+
+	}
+
+
+
+	public boolean ifBottoneGiaCliccato(JButton b){
+		boolean trovato=false;
+		Iterator <JButton> j = buttonsclick.iterator();
+
+		while(j.hasNext() && trovato==false){
+			if(b.equals(j.next())){
+				trovato=true;
+			}
+		}
+
+		return trovato;
 	}
 }
