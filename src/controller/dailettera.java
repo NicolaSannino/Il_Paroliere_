375ef625d1e204ef5a4379219f3927502f26c607
AlbertoTarabella <alberto.tarabella@edu.cobianchi.it>
2023-05-16 10:10:51 +0200

    implementazione algoritmo creazione tabella2

---
+++ src/controller/dailettera.java
@@ -0,0 +1,60 @@
+package controller;
+
+public class dailettera {
+    public static int controllo(int num){
+        if(num<11492){
+            return 97;
+        }else if(num<13380){
+            return 98;
+        }else if(num<18256){
+            return 99;
+        }else if(num<20606){
+            return 100;
+        }else if(num<31258){
+            return 101;
+        }else if(num<32650){
+            return 102;
+        }else if(num<35151){
+            return 103;
+        }else if(num<35937){
+            return 104;
+        }else if(num<48224){
+            return 105;
+        }else if(num<48227){
+            return 106;
+        }else if(num<48246){
+            return 107;
+        }else if(num<51738){
+            return 108;
+        }else if(num<55446){
+            return 109;
+        }else if(num<61446){
+            return 110;
+        }else if(num<69627){
+            return 111;
+        }else if(num<71978){
+            return 112;
+        }else if(num<72096){
+            return 113;
+        }else if(num<80744){
+            return 114;
+        }else if(num<87355){
+            return 115;
+        }else if(num<94388){
+            return 116;
+        }else if(num<96339){
+            return 117;
+        }else if(num<98442){
+            return 118;
+        }else if(num<98448){
+            return 119;
+        }else if(num<98461){
+            return 120;
+        }else if(num<98468){
+            return 121;
+        }else {
+            return 122;
+        }
+    }
+}
+
