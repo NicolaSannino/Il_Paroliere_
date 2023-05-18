ec22334af6ba7d381a0018fe37bd4fec07d4aa7a
AndreaComolli <andre.comolli@edu.cobianchi.it>
2023-05-18 11:19:06 +0200

    aggiunta controlli jtextfield nome utente e sistemazione grafica home

--- src/view/HomePage.java
+++ src/view/HomePage.java
@@ -5,6 +5,8 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.sql.SQLException;
 
 public class HomePage extends JFrame implements ActionListener {
@@ -13,7 +15,7 @@ public class HomePage extends JFrame implements ActionListener {
 
     private JComboBox<String> selectBox;
     private JPanel panelComboBox, ContBtn;
-    private JLabel titoloSelectBox;
+    private JLabel titoloSelectBox, titoloBoxImp, ErrorNome;
     private JTextField InsNome;
 
     String selectedOption = "Facile";
@@ -23,7 +25,8 @@ public class HomePage extends JFrame implements ActionListener {
         this.setTitle("HomePage il Paroliere");
         this.setSize(1100, 700);
 
-        Border bordo1 = BorderFactory.createLineBorder(Color.black, 3);
+        Border bordo1 = BorderFactory.createLineBorder(Color.black, 4);
+        Border bordo2 = BorderFactory.createLineBorder(Color.black, 3);
         Font fontA = new Font ("MV Boli", Font. BOLD, 20);
 
         //======================================================================================================
@@ -46,30 +49,63 @@ public class HomePage extends JFrame implements ActionListener {
         // CREAZIONE SELECT BOX DIFFICOLTA E INSERIMENTO NOME UTENTE
         //======================================================================================================
 
+        titoloBoxImp = new JLabel();
+        titoloBoxImp.setText("Impostazioni Paroliere");
+        titoloBoxImp.setHorizontalAlignment(JLabel.CENTER);
+        titoloBoxImp.setFont(new Font("MV Boli", Font.BOLD, 25));
+        titoloBoxImp.setBounds(150,15,300,30);
+
+        titoloSelectBox = new JLabel();
+        titoloSelectBox.setText("Seleziona La Difficolta");
+        titoloSelectBox.setHorizontalAlignment(JLabel.CENTER);
+        titoloSelectBox.setFont(new Font("MV Boli", Font.BOLD, 15));
+        titoloSelectBox.setBounds(150,90,300,30);
+
         String[] options = {"Facile", "Medio", "Difficile"};
         selectBox = new JComboBox<>(options);
         selectBox.addActionListener(this);
+        selectBox.setBounds(200,120,200,30);
 
-        titoloSelectBox = new JLabel();
-        titoloSelectBox.setText("Seleziona la difficoltà");
-        titoloSelectBox.setFont(new Font("MV Boli", Font.BOLD, 25));
+        ErrorNome = new JLabel();
+        ErrorNome.setText("Inserisci Nome Utente");
+        ErrorNome.setHorizontalAlignment(JLabel.CENTER);
+        ErrorNome.setFont(new Font("MV Boli", Font.BOLD, 20));
+        ErrorNome.setForeground(Color.RED);
+        ErrorNome.setBounds(150,190,300,30);
+        ErrorNome.setVisible(false);
 
         InsNome = new JTextField("Nome Utente");
-        InsNome.setBorder(bordo1);
-        InsNome.setSize(250,30);
+        InsNome.setBounds(175,220,250,30);
         InsNome.setFont(fontA);
-        InsNome.setForeground(Color.BLACK);
-        InsNome.setOpaque(false);
+        InsNome.setBackground(Color.BLACK);
+        InsNome.setForeground(Color.WHITE);
+        InsNome.setBorder(bordo2);
+        InsNome.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent e) {
+                if (InsNome.getText().equals("Nome Utente")) {
+                    InsNome.setText("");
+                    InsNome.setForeground(Color.WHITE);
+                }
+            }
+
+            public void focusLost(FocusEvent e) {
+
+            }
+        });
+
         InsNome.setVisible(true);
-        centerComponent(this, InsNome, 350);
 
         panelComboBox = new JPanel();
-        panelComboBox.setSize(300, 100);
+        panelComboBox.setSize(600, 300);
         panelComboBox.setBackground(new Color(123, 50, 250));
-        panelComboBox.setLayout(new FlowLayout());
+        panelComboBox.setLayout(null);
+        panelComboBox.add(titoloBoxImp);
         panelComboBox.add(titoloSelectBox);
         panelComboBox.add(selectBox);
-        this.centerComponent(this, panelComboBox, 230);
+        panelComboBox.add(ErrorNome);
+        panelComboBox.add(InsNome);
+        panelComboBox.setBorder(bordo1);
+        this.centerComponent(this, panelComboBox, 130);
 
         //======================================================================================================
         // BOTTONI PER AVVIARE PARTITA O PAGINA STATISTICHE
@@ -110,7 +146,7 @@ public class HomePage extends JFrame implements ActionListener {
         this.add(labelTitolo);
         this.add(panelComboBox);
         this.add(ContBtn);
-        this.add(InsNome);
+        //this.add(InsNome);
 
         this.setLayout(null);
         this.setResizable(false);
@@ -147,25 +183,31 @@ public class HomePage extends JFrame implements ActionListener {
 
         if (e.getSource() == btnOpenGioco){
 
-            if(selectedOption.equals("Facile")){
-                CampoGioco newPage = new CampoGioco(6);
-            }
+            if ((InsNome.getText().equals("")) || (InsNome.getText().equals("Nome Utente"))){
+                ErrorNome.setVisible(true);
+            }else{
+                if(selectedOption.equals("Facile")){
+                    CampoGioco newPage = new CampoGioco(6);
+                }
 
-            if(selectedOption.equals("Medio")){
-                CampoGioco newPage = new CampoGioco(5);
-            }
+                if(selectedOption.equals("Medio")){
+                    CampoGioco newPage = new CampoGioco(5);
+                }
 
-            if(selectedOption.equals("Difficile")){
-                CampoGioco newPage = new CampoGioco(4);
-            }
+                if(selectedOption.equals("Difficile")){
+                    CampoGioco newPage = new CampoGioco(4);
+                }
 
-            this.dispose();
+                this.dispose();
+            }
         }
 
         if(e.getSource() == btnStatistiche){
 
             try {
+
                 PageStatistiche pagStat = new PageStatistiche();
+
             } catch (SQLException ex) {
                 System.out.println("Non ci sono partite");
                 throw new RuntimeException(ex);
